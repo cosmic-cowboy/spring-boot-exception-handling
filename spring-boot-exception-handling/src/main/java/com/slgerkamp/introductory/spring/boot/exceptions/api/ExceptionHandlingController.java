c4c80660ecb364e4bd1c8fb0f9a88c8eef108912
hiroo takaku <slgerkamp@gmail.com>
2015-03-08 17:50:08 +0900

    Controller独自のエラーハンドリング：ステータスコードを変更

--- spring-boot-exception-handling/src/main/java/com/slgerkamp/introductory/spring/boot/exceptions/api/ExceptionHandlingController.java
+++ spring-boot-exception-handling/src/main/java/com/slgerkamp/introductory/spring/boot/exceptions/api/ExceptionHandlingController.java
@@ -2,7 +2,10 @@ package com.slgerkamp.introductory.spring.boot.exceptions.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.slgerkamp.introductory.spring.boot.exceptions.exception.OrderNotFoundException;
@@ -41,5 +44,27 @@ public class ExceptionHandlingController {
 		logger.info("Throw OrderNotFoundException");
 		throw new OrderNotFoundException();
 	}
+	
+	/**
+	 * ステータスコード409が返却される
+	 * @return
+	 */
+	@RequestMapping("/order/{id}")
+	String throwIllegalArgumentException(){
+		throw new IllegalArgumentException();
+	}
 
+	/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+	/* このController独自のエラーハンドリング
+	/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+	/**
+	 *  IllegalArgumentExceptionがスローされたら、409を返却する
+	 */
+	@ResponseStatus(value=HttpStatus.CONFLICT, reason = "IDが重複しています")
+	@ExceptionHandler(IllegalArgumentException.class)
+	public void conflict(){
+		logger.error("Request raised a IllegalArgumentException");
+	}
+	
 }	
